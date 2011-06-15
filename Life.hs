0406baf1a81d7cd28ccb82fa401ade05d7b73ad7
Alex Midgley <alex@tapdancinggoats.com>
2011-06-14 19:46:03 -0500

    Removed height property from World. Added comments.

--- Life.hs
+++ Life.hs
@@ -20,36 +20,42 @@ type Population = V.Vector Cell
 data World = World
              {
                worldWidth :: Int,
-               worldHeight :: Int,
                population :: Population
              }
 
+-- Get a position from a population index
 posFromIdx :: Int -> Int -> Position
 posFromIdx width idx =
     let yy = floor (fromIntegral idx / fromIntegral width)
         xx = idx `mod` width
     in (xx, yy)
 
+-- Get a population index from a position
 idxFromPos :: Int -> Position -> Int
 idxFromPos width (xx, yy) = width * yy + xx
 
+-- Map a function over a world's population
 mapWorld :: (Position -> Cell -> a) -> World -> V.Vector a
-mapWorld fn (World width height pop) = V.generate (V.length pop) mapCell
+mapWorld fn (World width pop) = V.generate (V.length pop) mapCell
     where mapCell idx = fn (posFromIdx width idx) (pop V.! idx)
 
+-- Render a world
 renderWorld :: World -> Picture
 renderWorld = Pictures . V.toList . mapWorld renderCell
 
+-- Render a cell
 renderCell :: Position -> Cell -> Picture
 renderCell (xx, yy) (Cell live)
     | live = Translate (fromIntegral xx) (fromIntegral yy) $ rectangleSolid 1 1
     | otherwise = Blank
 
+-- Get one cell from a world
 getCell :: World -> Position -> Cell
-getCell (World width _ pop) pos = pop V.! idxFromPos width pos
+getCell (World width pop) pos = pop V.! idxFromPos width pos
 
+-- Get the cells around one cell
 neighborCells :: World -> Position -> [Cell]
-neighborCells (World width _ pop) (px, py) = map (pop V.!) adjacentIndices
+neighborCells (World width pop) (px, py) = map (pop V.!) adjacentIndices
     where
       deltas = [-1, 0, 1]
       len = V.length pop
@@ -57,12 +63,14 @@ neighborCells (World width _ pop) (px, py) = map (pop V.!) adjacentIndices
       adjacentIndices = filter inBounds . map (\(dx, dy) -> ((py + dy) * width) + (px + dx)) $
                         [(x, y) | x <- deltas, y <- deltas, not (x == 0 && y == 0)]
 
+-- Count the number of live cells in a list
 countLiveCells :: [Cell] -> Int
 countLiveCells = foldl' live 0
     where live acc (Cell cell_live)
               | cell_live = acc + 1
               | otherwise = acc
 
+-- Age a cell using Conway's life algorithm
 ageCell :: World -> Position -> Cell -> Cell
 ageCell world pos (Cell cell_live) =
     let liveNeighbors = countLiveCells $ neighborCells world pos
@@ -71,14 +79,14 @@ ageCell world pos (Cell cell_live) =
         spawn = liveNeighbors == 3 && not cell_live
     in Cell $ spawn || (not lonely && not crowded && cell_live)
 
+-- Age all the cells in a world one step
 stepWorld :: World -> World
 stepWorld world = world {population = mapWorld (ageCell world) world}
 
+-- Create a new, empty world
 newWorld :: Int -> Int -> World
-newWorld width height = World width height $ V.replicate (width * height) (Cell False)
-    where xrange = [0..(width - 1)]
-          yrange = [0..(height - 1)]
+newWorld width height = World width $ V.replicate (width * height) (Cell False)
 
+-- Create a new world from a population
 newWorld' :: Int -> Population -> World
-newWorld' width cells = World width height cells
-    where height = floor $ fromIntegral (V.length cells) / fromIntegral width
+newWorld' width cells = World width cells
